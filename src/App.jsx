b4e9b17fe6bab9433a0afda91b5e64e208edde3f
Ganapathy Karthikeyan <rganapathykarthikeyan8@gmail.com>
2024-04-22 12:53:48 +0530

    Modified to get data from the API

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
       setIsLoading(true);
       const fd = new FormData();
       fd.append('file', file);
-      let response = await fetch('http://13.233.133.188:8000/parse_invoice_ocr/', { method: 'POST', body: fd })
+      let response = await fetch('http://13.126.100.8:8000/parse_invoice_ocr/', { method: 'POST', body: fd })
       let res = await response.json()
       setInvoiceData(res)
     }
@@ -42,42 +42,42 @@ function App() {
 
   // console.log(invoiceData && typeof invoiceData['Items purchased'])
 
-  const invoiceData1 = {
-    invoice_number: '132165',
-    invoice_date: '12/25/2024',
-    total_tax: '123654',
-    invoice_amount: '6549873',
-    buyer_name: 'Buyer',
-    buyer_address: 'BuyerAdd',
-    buyer_phone_number: '9876543210',
-    seller_name: 'Sell',
-    seller_address: 'DubaiMainRoad',
-    seller_phone_number: 'asdf',
-    // 'Items purchased': 'data'
-    // 'Items purchased': {
-    //   item_name: 'chalk',
-    //   tax: '135',
-    //   amount: '65497',
-    //   quantitiy: '1'
-    // }
-    'Items purchased': [{
-      item_name: 'chalk',
-      tax: '135',
-      amount: '65497',
-      quantitiy: '1'
-    }, {
-      item_name: 'bus',
-      tax: '135',
-      amount: '625497',
-      quantitiy: '1'
-    }, {
-      item_name: 'water',
-      tax: '13',
-      amount: '6497',
-      quantitiy: '12'
-    }]
-    // 'Items purchased': ['asdf', 'bwe', 'qhenre']
-  }
+  // const invoiceData1 = {
+  //   invoice_number: '132165',
+  //   invoice_date: '12/25/2024',
+  //   total_tax: '123654',
+  //   invoice_amount: '6549873',
+  //   buyer_name: 'Buyer',
+  //   buyer_address: 'BuyerAdd',
+  //   buyer_phone_number: '9876543210',
+  //   seller_name: 'Sell',
+  //   seller_address: 'DubaiMainRoad',
+  //   seller_phone_number: 'asdf',
+  // 'Items purchased': 'data'
+  // 'Items purchased': {
+  //   item_name: 'chalk',
+  //   tax: '135',
+  //   amount: '65497',
+  //   quantitiy: '1'
+  // }
+  // 'Items purchased': [{
+  //   item_name: 'chalk',
+  //   tax: '135',
+  //   amount: '65497',
+  //   quantitiy: '1'
+  // }, {
+  //   item_name: 'bus',
+  //   tax: '135',
+  //   amount: '625497',
+  //   quantitiy: '1'
+  // }, {
+  //   item_name: 'water',
+  //   tax: '13',
+  //   amount: '6497',
+  //   quantitiy: '12'
+  // }]
+  // 'Items purchased': ['asdf', 'bwe', 'qhenre']
+  // }
 
   return (
     <div className='h-screen w-screen'>
@@ -117,7 +117,7 @@ function App() {
         </div>
       </section>
       {
-        // invoiceData !== null &&
+        invoiceData !== null &&
         <section className=' sm:p-10 p-4'>
           <h1 className='text-4xl font-bold text-center p-6 text-[#733BDB]'>Invoice Details</h1>
           <div className='flex flex-col gap-5'>
@@ -127,21 +127,21 @@ function App() {
                 <div>
                   <div className='flex flex-col w-full gap-1'>
                     <label htmlFor="invoice_number" className='text-sm text-[#3b3b3b]'>Invoice Number</label>
-                    <input type='text' id='invoice_number' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' placeholder='Invoice Number' value={invoiceData1.invoice_number} />
+                    <input type='text' id='invoice_number' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' placeholder='Invoice Number' value={invoiceData.invoice_number} />
                   </div>
                   <div className='flex flex-col w-full gap-1'>
                     <label htmlFor="invoice_date" className='text-sm text-[#3b3b3b]'>Invoice Date</label>
-                    <input type='text' id='invoice_date' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' placeholder='Invoice Date' value={invoiceData1.invoice_date} />
+                    <input type='text' id='invoice_date' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' placeholder='Invoice Date' value={invoiceData.invoice_date} />
                   </div>
                 </div>
                 <div>
                   <div className='flex flex-col w-full gap-1'>
                     <label htmlFor="total_tax" className='text-sm text-[#3b3b3b]'>Total Tax</label>
-                    <input type='text' id='total_tax' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' placeholder='Total Tax' value={invoiceData1.total_tax} />
+                    <input type='text' id='total_tax' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' placeholder='Total Tax' value={invoiceData.total_tax} />
                   </div>
                   <div className='flex flex-col w-full gap-1'>
                     <label htmlFor="invoice_amount" className='text-sm text-[#3b3b3b]'>Invoice Amount</label>
-                    <input type='text' id='invoice_amount' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' placeholder='Invoice Amount' value={invoiceData1.invoice_amount} />
+                    <input type='text' id='invoice_amount' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' placeholder='Invoice Amount' value={invoiceData.invoice_amount} />
                   </div>
                 </div>
               </section>
@@ -152,15 +152,15 @@ function App() {
                 <section className='flex flex-grow flex-col p-2 border-slate-400 border rounded-md shadow-lg'>
                   <div className='flex flex-col w-full gap-1'>
                     <label htmlFor="buyer_name" className='text-sm text-[#3b3b3b]'>Buyer Name/Consignee</label>
-                    <input type='text' id='buyer_name' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' placeholder='Buyer Name/Consignee' value={invoiceData1.buyer_name} />
+                    <input type='text' id='buyer_name' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' placeholder='Buyer Name/Consignee' value={invoiceData.buyer_name} />
                   </div>
                   <div className='flex flex-col w-full gap-1'>
                     <label htmlFor="buyer_address" className='text-sm text-[#3b3b3b]'>Buyer Address</label>
-                    <input type='text' id='buyer_address' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' placeholder='Buyer Address' value={invoiceData1.buyer_address} />
+                    <input type='text' id='buyer_address' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' placeholder='Buyer Address' value={invoiceData.buyer_address} />
                   </div>
                   <div className='flex flex-col w-full gap-1'>
                     <label htmlFor="buyer_phone_number" className='text-sm text-[#3b3b3b]'>Buyer Phone Number</label>
-                    <input type='text' id='buyer_phone_number' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' placeholder='Buyer Phone Number' value={invoiceData1.buyer_phone_number} />
+                    <input type='text' id='buyer_phone_number' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' placeholder='Buyer Phone Number' value={invoiceData.buyer_phone_number} />
                   </div>
                 </section>
               </div>
@@ -169,52 +169,55 @@ function App() {
                 <section className='flex flex-grow flex-col p-2 border-slate-400 border rounded-md shadow-lg'>
                   <div className='flex flex-col w-full gap-1'>
                     <label htmlFor="seller_name" className='text-sm text-[#3b3b3b]'>Seller Name</label>
-                    <input type='text' id='seller_name' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' placeholder='Seller Name' value={invoiceData1.seller_name} />
+                    <input type='text' id='seller_name' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' placeholder='Seller Name' value={invoiceData.seller_name} />
                   </div>
                   <div className='flex flex-col w-full gap-1'>
                     <label htmlFor="seller_address" className='text-sm text-[#3b3b3b]'>Seller Address</label>
-                    <input type='text' id='seller_address' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' placeholder='Seller Address' value={invoiceData1.seller_address} />
+                    <input type='text' id='seller_address' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' placeholder='Seller Address' value={invoiceData.seller_address} />
                   </div>
                   <div className='flex flex-col w-full gap-1'>
                     <label htmlFor="seller_phone_number" className='text-sm text-[#3b3b3b]'>Seller Phone Number</label>
-                    <input type='text' id='seller_phone_number' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' placeholder='Seller Phone Number' value={invoiceData1.seller_phone_number} />
+                    <input type='text' id='seller_phone_number' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' placeholder='Seller Phone Number' value={invoiceData.seller_phone_number} />
                   </div>
                 </section>
               </div>
             </section>
-            <section className='flex flex-grow flex-col p-2 border-slate-400 border rounded-md shadow-lg'>
-              <div className='flex flex-col w-full gap-1'>
-                <label htmlFor="Items purchased" className='text-sm text-[#3b3b3b]'>Items purchased</label>
-                {
-                  typeof invoiceData1['Items purchased'] === 'object' &&
-                  typeof invoiceData1['Items purchased'] !== 'string' &&
-                  Array.isArray(invoiceData1['Items purchased']) &&
-                  typeof invoiceData1['Items purchased'][0] === 'object' &&
-                  typeof invoiceData1['Items purchased'][0] !== 'string' &&
-                  !Array.isArray(typeof invoiceData1['Items purchased'][0]) &&
-                  <TableMultiComponent items={invoiceData1['Items purchased']} />
-                }
-                {typeof invoiceData1['Items purchased'] === 'object' && typeof invoiceData1['Items purchased'] !== 'string' ?
-                  Array.isArray(invoiceData1['Items purchased']) ?
-                    invoiceData1['Items purchased'].map((item, index) => {
-                      if (typeof item === 'string') {
-                        return <input type='text' id='seller_phone_number' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' key={index} placeholder={'Items' + index} value={item} />
-                      }
-                      else if (typeof item === 'object' && Array.isArray(item)) {
-                        item.map((subitem) => {
-                          return <input type='text' id='seller_phone_number' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' key={index} placeholder={'Items' + index} value={subitem} />
-                        })
-                      }
-                    }) :
-                    <TableSingleComponent item={invoiceData1['Items purchased']} />
-                  : typeof invoiceData1['Items purchased'] === 'string' ? <></> :
-                    Object.entries(invoiceData1['Items purchased']).map(([k, v]) => {
-                      return <p key={k}>The key {k} has a value of {v}</p>
-                    })}
-                {/* String */}
-                {typeof invoiceData1['Items purchased'] === 'string' && <input type='text' id='Items purchased' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' placeholder='Items purchased' value={invoiceData1['Items purchased']} />}
+            <section>
+              <h4 className='text-[#733BDB] font-light font-mono text-lg'>Items Details</h4>
+              <section className='flex flex-grow flex-col p-2 border-slate-400 border rounded-md shadow-lg'>
+                <div className='flex flex-col w-full gap-1'>
+                  <label htmlFor="Items purchased" className='text-sm text-[#3b3b3b]'>Items purchased</label>
+                  {
+                    typeof invoiceData['Items purchased'] === 'object' &&
+                    typeof invoiceData['Items purchased'] !== 'string' &&
+                    Array.isArray(invoiceData['Items purchased']) &&
+                    typeof invoiceData['Items purchased'][0] === 'object' &&
+                    typeof invoiceData['Items purchased'][0] !== 'string' &&
+                    !Array.isArray(typeof invoiceData['Items purchased'][0]) &&
+                    <TableMultiComponent items={invoiceData['Items purchased']} />
+                  }
+                  {typeof invoiceData['Items purchased'] === 'object' && typeof invoiceData['Items purchased'] !== 'string' ?
+                    Array.isArray(invoiceData['Items purchased']) ?
+                      invoiceData['Items purchased'].map((item, index) => {
+                        if (typeof item === 'string') {
+                          return <input type='text' id='seller_phone_number' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' key={index} placeholder={'Items' + index} value={item} />
+                        }
+                        else if (typeof item === 'object' && Array.isArray(item)) {
+                          item.map((subitem) => {
+                            return <input type='text' id='seller_phone_number' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' key={index} placeholder={'Items' + index} value={subitem} />
+                          })
+                        }
+                      }) :
+                      <TableSingleComponent item={invoiceData['Items purchased']} />
+                    : typeof invoiceData['Items purchased'] === 'string' ? <></> :
+                      Object.entries(invoiceData['Items purchased']).map(([k, v]) => {
+                        return <p key={k}>The key {k} has a value of {v}</p>
+                      })}
+                  {/* String */}
+                  {typeof invoiceData['Items purchased'] === 'string' && <input type='text' id='Items purchased' className='w-full p-2 bg-[#f9f9f9] border-b border-[#3b3b3b] text-[#3b3b3b] placeholder:text-[#3b3b3b]' placeholder='Items purchased' value={invoiceData['Items purchased']} />}
 
-              </div>
+                </div>
+              </section>
             </section>
           </div>
           <div className='py-6 flex flex-row w-full justify-center gap-10'>
